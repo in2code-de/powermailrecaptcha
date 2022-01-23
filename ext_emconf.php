7452f583cb5fc180c5b2381e8393051a3878de62
Philipp Kitzberger <philipp@kitze.net>
2022-01-23 18:58:43 +0100

    Bumped dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'powermail' => '8.0.0-8.99.99',
-            'typo3' => '10.0.0-10.99.99'
+            'powermail' => '8.0.0-9.99.99',
+            'typo3' => '10.0.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
